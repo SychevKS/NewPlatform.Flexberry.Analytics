ae9f394301b372ca1452c3e91ae464725d77f2b9
Alexey Kostarev <kaf@ics.perm.ru>
2018-12-04 18:11:50 +0500

    Add Powershell script

---
+++ pentaho/getPentaho.ps1
@@ -0,0 +1,14 @@
+$documentsPath = [Environment]::GetFolderPath('MyDocuments')
+$folderName = 'Pentaho'
+cd $documentsPath
+New-Item -ErrorAction Ignore -Path $folderName -ItemType 'directory'
+cd $folderName
+$client = new-object System.Net.WebClient
+$downloadPath = 'https://github.com/Flexberry/NewPlatform.Flexberry.Analytics/tree/master/pentaho'
+$client.DownloadFile("$downloadPath/.env", "$pwd\.env")
+$client.DownloadFile("$downloadPath/pull.sh", "$pwd\pull.cmd")
+$client.DownloadFile("$downloadPath/docker-compose.yml", "$pwd\docker-compose.yml")
+$client.DownloadFile("$downloadPath/composeStart.sh", "$pwd\composeStart.cmd")
+$client.DownloadFile("$downloadPath/composeStart.sh", "$pwd\composeStart.cmd")
+$client.DownloadFile("$downloadPath/swarmStart.sh", "$pwd\swarmStart.cmd")
+$client.DownloadFile("$downloadPath/swarmStart.sh", "$pwd\swarmStart.cmd")
