4be9623b868fd607bc3ef0074c6e6da7db988953
Масалкин Павел <pashamasalkin@yandex.ru>
2018-12-14 10:02:05 +0500

    Correct link

--- pentaho/getPentaho.ps1
+++ pentaho/getPentaho.ps1
@@ -4,7 +4,7 @@ cd $documentsPath
 New-Item -ErrorAction Ignore -Path $folderName -ItemType 'directory'
 cd $folderName
 $client = new-object System.Net.WebClient
-$downloadPath = 'https://github.com/Flexberry/NewPlatform.Flexberry.Analytics/tree/master/pentaho'
+$downloadPath = 'https://raw.githubusercontent.com/Flexberry/NewPlatform.Flexberry.Analytics/master/pentaho'
 $client.DownloadFile("$downloadPath/.env", "$pwd\.env")
 $client.DownloadFile("$downloadPath/pull.sh", "$pwd\pull.cmd")
 $client.DownloadFile("$downloadPath/docker-compose.yml", "$pwd\docker-compose.yml")
